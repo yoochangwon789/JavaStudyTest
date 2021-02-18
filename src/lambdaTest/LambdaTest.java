eb9890e621d8cf57edaac97c8fb40c3228be8eb2
changwon <ckddnjs554@naver.com>
2021-02-18 23:17:33 +0900

    Feat : 람다식 활용 방법

--- src/lambdaTest/LambdaTest.java
+++ src/lambdaTest/LambdaTest.java
@@ -10,5 +10,22 @@ public class LambdaTest {
         // 람다식을 변수명으로 활용
         printString lambdaStr = str -> System.out.println(str);
         lambdaStr.showString("람다식을 변수명으로 활용");
+
+        // 매개변수 타입 활용
+        showMyString(lambdaStr);
+
+        // 람다식을 반환 값으로 활용
+        printString lambdaStr2 = returnString();
+        lambdaStr2.showString("람다식을 반환 값으로 활용");
+    }
+
+    // 람다식 함수의 매개변수 타입으로 활용
+    public static void showMyString(printString p) {
+        p.showString("람다식을 매개변수 타입으로 활용");
+    }
+
+    // 람다식을 반환 값으로 사용
+    public static printString returnString() {
+        return str -> System.out.println(str + "!!!");
     }
 }
