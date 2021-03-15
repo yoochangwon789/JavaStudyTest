0bbfc14cb794090d2f5f457bd40ae67c1dac84cc
changwon <ckddnjs554@naver.com>
2021-03-15 21:53:55 +0900

    Feat : Stream 을 이용한 배열 출력 방식

--- src/streamTest/StreamTest.java
+++ src/streamTest/StreamTest.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public class StreamTest {
     public static void main(String[] args) {
-
+        
+        // stream 을 이용한 배열 출력 방식
         int[] arr = {1,2,3,4,5};
 
         int cum = Arrays.stream(arr).sum();
